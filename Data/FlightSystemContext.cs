7d0a00e021c82650fbcd45877d20205c84b195b5
PanXDuong <xuanduongtien@gmail.com>
2024-04-17 16:34:13 +0200

    Add dbcontext and seeding data

---
+++ Data/FlightSystemContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System_Zarzadzania_Lotami.Data.EntitiesConfig;
+using System_Zarzadzania_Lotami.Data.Entity;
+
+namespace System_Zarzadzania_Lotami.Data
+{
+    public class FlightSystemContext : DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<PlaneType> Types { get; set; }
+
+        public FlightSystemContext(DbContextOptions<FlightSystemContext> options) : base(options) { }
+
+/*        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("FlightDataBase");
+        }*/
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new FlightConfig());
+            modelBuilder.ApplyConfiguration(new LocationConfig());
+            modelBuilder.ApplyConfiguration(new PlaneTypeConfig());
+
+            new SeedingData(modelBuilder).Seed();
+        }
+    }
+}
---
+++ Data/SeedingData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using System_Zarzadzania_Lotami.Data.Entity;
+
+namespace System_Zarzadzania_Lotami.Data
+{
+    public class SeedingData
+    {
+        private readonly ModelBuilder _modelBuilder;
+
+        public SeedingData(ModelBuilder modelBuilder)
+        {
+            _modelBuilder = modelBuilder;
+        }
+
+        public void Seed()
+        {
+            _modelBuilder.Entity<Location>().HasData(
+            new Location { Id = 1, CityName = "New York" },
+            new Location { Id = 2, CityName = "Warsaw" },
+            new Location { Id = 3, CityName = "Gdansk" },
+            new Location { Id = 4, CityName = "Krakow" }
+            );
+
+            _modelBuilder.Entity<PlaneType>().HasData(
+            new PlaneType { Id = 1, TypeName = "Boeing" },
+            new PlaneType { Id = 2, TypeName = "Airbus" },
+            new PlaneType { Id = 3, TypeName = "Embraer"}
+            );
+
+            _modelBuilder.Entity<Flight>().HasData(
+            new Flight
+            {
+                Id = 1,
+                FlightNumber = 4534,
+                DepartureDate = new DateTime(2024, 4, 20, 8, 30, 0),
+                DepartureFrom = 4,
+                ArrivalTo = 2,
+                PlaneTypeId = 3
+            },
+            new Flight
+            {
+                Id = 2,
+                FlightNumber = 2432,
+                DepartureDate = new DateTime(2024, 4, 22, 18, 0, 0),
+                DepartureFrom = 3,
+                ArrivalTo = 1,
+                PlaneTypeId = 1
+            },
+            new Flight
+            {
+                Id = 3,
+                FlightNumber = 3464,
+                DepartureDate = new DateTime(2024, 4, 21, 4, 45, 0),
+                DepartureFrom = 1,
+                ArrivalTo = 2,
+                PlaneTypeId = 2
+            }
+           );
+        }
+    }
+}
